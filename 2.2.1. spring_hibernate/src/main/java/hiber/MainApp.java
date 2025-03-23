df91db129f469462419f0fb4f616a78a46a0eb63
a-levyakov <a.levyakov95@gmail.com>
2025-03-23 17:59:57 +0300

    1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,11 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("car1", 1)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("car2", 2)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("car3", 3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("car4",4)));
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -28,6 +30,9 @@ public class MainApp {
          System.out.println("Email = "+user.getEmail());
          System.out.println();
       }
+
+      User user = userService.getUserByCar("car3", 3);
+      System.out.println(user.getFirstName());
 
       context.close();
    }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -19,13 +19,21 @@ public class User {
    @Column(name = "email")
    private String email;
 
-   public User() {}
-   
-   public User(String firstName, String lastName, String email) {
+
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "car")
+   private Car car;
+
+   public User() {
+   }
+
+   public User(String firstName, String lastName, String email, Car car) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+      this.car = car;
    }
+
 
    public Long getId() {
       return id;
